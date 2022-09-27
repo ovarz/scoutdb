f566d80d39b5a746a3b8f753d6806764ed2d3dc4
ovarz <aufar.mahardi@viva.co.id>
2022-09-27 14:24:05 +0700

    a

---
+++ practice/team/player/speed/index.php
@@ -0,0 +1,44 @@
+<?php 
+  $page='dashboard';
+  $channel='practice';
+  require ('../../../../inc/base.php')
+?>
+<?php require ($_SERVER['SCOUTDB'].'inc/data.php')?>
+<?php require ($_SERVER['SCOUTDB'].'inc/meta.php')?>
+<?php require ($_SERVER['SCOUTDB'].'inc/header.php')?>
+<?php require ($_SERVER['SCOUTDB'].'inc/menu-main.php')?>
+<link rel="stylesheet" type="text/css" href="css/practice.css"/>
+<div class="rancak-container">
+  <span class="width-max">
+
+    <section aria-label="Breadcrumb" class="breadcrumb">
+      <a aria-label="Home" title="Home" class="breadcrumb-link content_center" href="home.php">
+        <?php require ($_SERVER['SCOUTDB'].'img/icon/home.svg')?>
+      </a>
+      <a aria-label="Practice" title="Practice" class="breadcrumb-link content_center" href="practice/">
+        <span>Practice</span>
+      </a>
+      <a aria-label="Team Full Name FC" title="Team Full Name FC" class="breadcrumb-link content_center" href="practice/team/">
+        <span>Team_Full_Name FC</span>
+      </a>
+      <a aria-label="Team Full Name FC" title="Team Full Name FC" class="breadcrumb-link content_center" href="practice/team/player/">
+        <span>Player_Full_Name</span>
+      </a>
+      <div class="breadcrumb-link content_center">
+        <span>Speed</span>
+      </div>
+    </section>
+	
+	
+
+    <section aria-label="Section Practice" class="section-container">
+      <div class="section-title">
+	    <div class="section-title-label">Speed</div>
+	  </div>
+      
+    </section>
+	
+  </span>
+</div>
+<?php require ($_SERVER['SCOUTDB'].'inc/footer.php')?>
+<?php require ($_SERVER['SCOUTDB'].'inc/base-bottom.php')?>
