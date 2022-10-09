f073ae71e5442c4495ee0dce1c6c3e13b3af96f7
ovarz <ovarz_03@yahoo.com>
2022-10-09 20:36:50 +0700

    practice endurance

---
+++ practice/team/player/endurance/result.php
@@ -0,0 +1,81 @@
+<?php 
+  $page='dashboard';
+  $menu='full';
+  $channel='practice';
+  $practice_type='speed';
+  $backto_link='practice/team/player/'.$practice_type.'/'; $backto_label=$practice_type;
+  require ('../../../../inc/base.php')
+?>
+<?php require ($_SERVER['SCOUTDB'].'inc/data.php')?>
+<?php require ($_SERVER['SCOUTDB'].'inc/meta.php')?>
+<?php $practice_result='practice'; require ($_SERVER['SCOUTDB'].'inc/header.php')?>
+<?php require ($_SERVER['SCOUTDB'].'inc/menu-main.php')?>
+<link rel="stylesheet" type="text/css" href="css/practice.css?<?php echo $anticache; ?>"/>
+<div class="rancak-container">
+  <span class="width-max">
+
+    <section aria-label="Breadcrumb" class="breadcrumb">
+      <a aria-label="Home" title="Home" class="breadcrumb-link content_center" href="home.php">
+        <?php require ($_SERVER['SCOUTDB'].'img/icon/home.svg')?>
+      </a>
+      <a aria-label="Practice" title="Practice" class="breadcrumb-link content_center" href="practice/">
+        <span>Practice</span>
+      </a>
+      <a aria-label="Team Full Name FC" title="Team Full Name FC" class="breadcrumb-link content_center" href="practice/team/">
+        <span>Team_Full_Name FC</span>
+      </a>
+      <a aria-label="Player_Full_Name" title="Player_Full_Name" class="breadcrumb-link content_center" href="practice/team/player/">
+        <span>Player_Full_Name</span>
+      </a>
+      <a aria-label="<?php echo $practice_type; ?>" title="<?php echo $practice_type; ?>" class="breadcrumb-link content_center" 
+	  href="practice/team/player/<?php echo $practice_type; ?>/">
+        <span><?php echo $practice_type; ?></span>
+      </a>
+    </section>
+	
+	
+
+    <section aria-label="Section Practice" class="section-container">
+      <h2 class="section-title">
+	    <div class="section-title-icon"><?php require ($_SERVER['SCOUTDB'].'img/icon/menu-practice.svg')?></div>
+	    <div class="section-title-label"><?php echo $practice_type; ?> - Practice Report</div>
+		<div class="practice-result-search">
+		  <input class="search-field" name="" type="text" placeholder="Search by player name.....">
+		  <button title="Search" class="search-button">
+		    <?php require ($_SERVER['SCOUTDB'].'img/icon/search.svg')?>
+		  </button>
+		</div>
+	  </h2>
+      <div class="report-practice-list">
+	    <?php for ($i=1; $i <= 20 ; $i++) { ?>
+	    <div class="rpl-box">
+		  <div class="rpl-photo">
+		    <div class="rpl-frame flex_ori thumb-loading">
+			  <img alt="img_title" class="lazyload" data-original="img/sample/player-<?php echo rand(1,20); ?>.jpg" />
+			</div>
+		  </div>
+		  <div class="rpl-info">
+		    <span>
+			  <div class="rpl-name"><?php echo $random_username[array_rand($random_username)]; ?></div>
+			  <div class="rpl-rating rating-icon">
+				<?php $training_rating_total=rand(1,5); for ($training_rating=1; $training_rating <= $training_rating_total ; $training_rating++) { ?>
+				  <?php require ($_SERVER['SCOUTDB'].'img/icon/star-filled.svg')?>
+				<?php } ?>
+				<?php for ($training_rating=1; $training_rating <= 5 - $training_rating_total ; $training_rating++) { ?>
+				  <?php require ($_SERVER['SCOUTDB'].'img/icon/star-outline.svg')?>
+				<?php } ?>
+			  </div>
+			  <div class="rpl-date">Last update : 00 Sep 0000 - 00:00</div>
+			</span>
+		  </div>
+		</div>
+		<?php } ?>
+      </div>
+	  <?php require ($_SERVER['SCOUTDB'].'module/pagination.php')?>
+    </section>
+	
+  </span>
+</div>
+<script defer rancak-hold="js/practice.js"></script>
+<?php require ($_SERVER['SCOUTDB'].'inc/footer.php')?>
+<?php require ($_SERVER['SCOUTDB'].'inc/base-bottom.php')?>
---
+++ practice/team/player/endurance/step3.php
@@ -0,0 +1,101 @@
+<?php 
+  $page='dashboard';
+  $menu='full';
+  $channel='practice';
+  $practice_type='speed';
+  $backto_link='practice/team/player/'.$practice_type.'/'; $backto_label=$practice_type;
+  require ('../../../../inc/base.php')
+?>
+<?php require ($_SERVER['SCOUTDB'].'inc/data.php')?>
+<?php require ($_SERVER['SCOUTDB'].'inc/meta.php')?>
+<?php $practice_result='none'; require ($_SERVER['SCOUTDB'].'inc/header.php')?>
+<?php require ($_SERVER['SCOUTDB'].'inc/menu-main.php')?>
+<link rel="stylesheet" type="text/css" href="css/practice.css?<?php echo $anticache; ?>"/>
+<div class="rancak-container">
+  <span class="width-max">
+
+    <section aria-label="Breadcrumb" class="breadcrumb">
+      <a aria-label="Home" title="Home" class="breadcrumb-link content_center" href="home.php">
+        <?php require ($_SERVER['SCOUTDB'].'img/icon/home.svg')?>
+      </a>
+      <a aria-label="Practice" title="Practice" class="breadcrumb-link content_center" href="practice/">
+        <span>Practice</span>
+      </a>
+      <a aria-label="Team Full Name FC" title="Team Full Name FC" class="breadcrumb-link content_center" href="practice/team/">
+        <span>Team_Full_Name FC</span>
+      </a>
+      <a aria-label="Player_Full_Name" title="Player_Full_Name" class="breadcrumb-link content_center" href="practice/team/player/">
+        <span>Player_Full_Name</span>
+      </a>
+      <a aria-label="<?php echo $practice_type; ?>" title="<?php echo $practice_type; ?>" class="breadcrumb-link content_center" href="practice/team/player/<?php echo $practice_type; ?>/">
+        <span><?php echo $practice_type; ?></span>
+      </a>
+    </section>
+	
+	
+
+    <section aria-label="Section Practice" class="section-container">
+      <h2 class="section-title">
+	    <div class="section-title-icon"><?php require ($_SERVER['SCOUTDB'].'img/icon/menu-practice.svg')?></div>
+	    <div class="section-title-label"><?php echo $practice_type; ?></div>
+	  </h2>
+      <div class="practice-tutorial practice-tutorial-process">
+        <div class="practice-tutorial-left">
+		  <?php require ($_SERVER['SCOUTDB'].'module/practice-player-profile.php')?>
+		  <div class="ptl-box ptl-comment">
+            <textarea class="plt-comment-box" placeholder="Masukkan komentar......"></textarea>
+			<button aria-label="Submit Comment" class="btn content_center">
+              <span>Submit Comment</span>
+            </button>
+		  </div>
+        </div>
+		
+		
+		
+        <div class="practice-tutorial-right">
+          <div class="practice-tutorial-sticky">
+		    <div class="practice-result-info">
+              <ul class="pri-row">
+                <li class="pri-label">Practice ID</li>
+                <li class="pri-data">#123456</li>
+              </ul>
+              <ul class="pri-row">
+                <li class="pri-label">Date & Time</li>
+                <li class="pri-data">00 September 0000 - 00:00</li>
+              </ul>
+			</div>
+		    <div class="practice-result-rating">
+  			  <div class="ptl-title">Rating Result</div>
+			  <div class="rating-icon">
+                <?php require ($_SERVER['SCOUTDB'].'img/icon/star-filled.svg')?>
+                <?php require ($_SERVER['SCOUTDB'].'img/icon/star-filled.svg')?>
+                <?php require ($_SERVER['SCOUTDB'].'img/icon/star-filled.svg')?>
+                <?php require ($_SERVER['SCOUTDB'].'img/icon/star-outline.svg')?>
+                <?php require ($_SERVER['SCOUTDB'].'img/icon/star-outline.svg')?>
+			  </div>
+			</div>
+            <div class="practice-countdown">
+			  <div class="practice-countdown-hours">00</div>
+			  <div class="practice-countdown-minutes">00</div>
+			  <div class="practice-countdown-seconds">13</div>
+			</div>
+            <div class="practice-tutorial-action">
+              <a aria-label="Cancel Practice" title="Cancel Practice" class="btn pta-button pta-button-cancel content_center" 
+              href="practice/team/player/<?php echo $practice_type; ?>/step1.php">
+                <span>Retake Practice</span>
+              </a>
+              <a aria-label="Start Practice" title="Start Practice" class="btn pta-button pta-button-start content_center" 
+              href="practice/team/player/<?php echo $practice_type; ?>/result.php">
+                <span>Submit Practice</span>
+              </a>
+            </div>
+          </div>    
+        </div>
+      </div>
+    </section>
+	
+  </span>
+</div>
+<script defer rancak-hold="js/practice.js"></script>
+<?php require ($_SERVER['SCOUTDB'].'inc/footer.php')?>
+<?php require ($_SERVER['SCOUTDB'].'inc/base-bottom.php')?>
