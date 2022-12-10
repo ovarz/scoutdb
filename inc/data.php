ed852d0ea79b45096ff2dd4da5089af6c7bfa04e
ovarz <ovarz_03@yahoo.com>
2022-12-10 09:31:20 +0700

    Update data.php

--- inc/data.php
+++ inc/data.php
@@ -8,7 +8,7 @@
   
   $random_keyword=array("Lorem ipsum dolor sit amet, consectetur adipiscing elit.","Nam erat orci, aliquam non nulla at, pretium feugiat lectus.","Nulla rhoncus venenatis lacus ut facilisis.","Mauris semper condimentum nisl quis blandit.","Curabitur tempus libero eu risus dapibus, id luctus tellus lacinia. ","Curabitur at molestie ex. Nulla hendrerit sem mauris, vel lobortis risus viverra et.","Pellentesque bibendum sem vitae diam euismod,","semper congue justo posuere.","Aenean condimentum ex justo, eu sagittis nibh vulputate eget.","Suspendisse potenti. Nam et urna vel urna efficitur malesuada non at nisi.","Mauris vel leo sed diam ultrices pretium semper a turpis.");
 
-  $random_username=array("Pay Joe","Rhou Jya Lee","Rush Die","Soul as Three");
+  $random_username=array("Pay Joe","Rhou Jya Lee","Rush Die","Soul as Three","Soo Lee Hyin");
 
   $random_position=array("Goalkeeper","Right Back","Right Wing Back","Left Back","Left Wing Back","Center Back","Defending Midfielder","Left Midfielder","Right Midfielder","Center Midfielder","Attacking Midfielder","Left Winger","Right Winger","Striker","Second Striker");
   
